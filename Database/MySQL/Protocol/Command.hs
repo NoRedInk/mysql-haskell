9d92d0ff82ece86b2cdd295c46a0fcecf20d6d37
benjmhart <benjmhart@gmail.com>
2020-06-23 16:41:50 -0400

    stack init and bump to 15.15

--- Database/MySQL/Protocol/Command.hs
+++ Database/MySQL/Protocol/Command.hs
@@ -17,6 +17,7 @@ module Database.MySQL.Protocol.Command where
 
 import           Control.Applicative
 import           Control.Monad
+import qualified Control.Monad.Fail as Fail
 import           Data.Binary
 import           Data.Binary.Get
 import           Data.Binary.Parser
@@ -25,6 +26,11 @@ import           Data.ByteString                    (ByteString)
 import qualified Data.ByteString.Lazy               as L
 import           Database.MySQL.Protocol.MySQLValue
 import           Database.MySQL.Protocol.Packet
+
+--TODO: orphan instance MonadFail Data.Binary
+instance MonadFail PutM where
+
+
 
 --------------------------------------------------------------------------------
 --  Commands
